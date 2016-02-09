928b2659b4a6e6d88cca1b4433e2bd6e2ca6b386
Andrew Wilson <andrewmoorewilson@gmail.com>
2016-02-09 16:54:24 -0500

    Add an integration test for uploading replays

---
+++ hsreplayarchive/test/base.py
@@ -0,0 +1,75 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+import os
+from datetime import datetime
+import json
+
+APP_DIR = os.path.dirname(os.path.abspath(__file__))
+SCREEN_DUMP_LOCATION = os.path.join(APP_DIR,'screendumps')
+JSON_DATA_LOCATION = os.path.join(APP_DIR,'json')
+REPLAY_DATA_LOCATION = os.path.join(APP_DIR, 'replays')
+FIXTURE_DATA_LOCATION = os.path.join(APP_DIR, 'fixtures')
+LOG_DATA_LOCATION = os.path.join(APP_DIR, 'logs')
+
+
+class FunctionalTest(StaticLiveServerTestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
+
+	def get_json_fixture(self, name):
+		name_with_extension = "%s.json" % name
+		path = os.path.join(JSON_DATA_LOCATION, name_with_extension)
+		if not os.path.exists(path):
+			raise FileExistsError("Could not locate: %s" % path)
+
+		return json.load(open(path))
+
+	def tearDown(self):
+		if self._test_has_failed():
+			if not os.path.exists(SCREEN_DUMP_LOCATION):
+				os.makedirs(SCREEN_DUMP_LOCATION)
+
+			for ix, handle in enumerate(self.browser.window_handles):
+				self._windowid = ix
+				self.browser.switch_to.window(handle)
+				self.take_screenshot()
+				self.dump_html()
+		self.browser.quit()
+		super().tearDown()
+
+	def _test_has_failed(self):
+		for method, error in self._outcome.errors:
+			if error:
+				return True
+		return False
+
+	def take_screenshot(self):
+		filename = self._get_filename() + '.png'
+		print('screenshotting to', filename)
+		self.browser.get_screenshot_as_file(filename)
+
+	def dump_html(self):
+		filename = self._get_filename() + '.html'
+		print('dumping page HTML to', filename)
+		with open(filename, 'w') as f:
+			f.write(self.browser.page_source)
+
+	def _get_filename(self):
+		timestamp = datetime.now().isoformat().replace(':', '.')[:19]
+		return '{folder}/{classname}.{method}-window{windowid}-{timestamp}'.format(
+			folder = SCREEN_DUMP_LOCATION,
+			classname = self.__class__.__name__,
+			method = self._testMethodName,
+			windowid = self._windowid,
+			timestamp = timestamp
+		)
+
+
+class TestDataConsumerMixin:
+	"""A mixin class for accessing test data."""
+
+	def replay_file_path(self, replay_name):
+		return os.path.join(REPLAY_DATA_LOCATION, replay_name)
+
---
+++ hsreplayarchive/web/tests/test_integration.py
@@ -0,0 +1,20 @@
+from test.base import FunctionalTest, TestDataConsumerMixin
+import requests
+
+
+class ReplayUploadTests(FunctionalTest, TestDataConsumerMixin):
+
+	def setUp(self):
+		super().setUp()
+
+	def test_replay_xml_upload(self):
+		with open(self.replay_file_path('2016-02-06T05.32.12.gamestate.4.log.xml')) as xml_file:
+			original_xml_data = xml_file.read()
+
+			server_url = '%s%s' % (self.live_server_url, '/api/v1/replay/upload')
+			response = requests.post(server_url, data=original_xml_data)
+
+			url_to_xml = response.content
+
+			xml_back_from_server = requests.get(url_to_xml).content
+			self.assertEqual(xml_back_from_server, original_xml_data)
