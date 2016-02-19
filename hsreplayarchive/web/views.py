da2ef81717bff76561ea2995bcca5c05ee50eb9e
Andrew Wilson <andrewmoorewilson@gmail.com>
2016-02-19 10:39:38 -0500

    Display replay player names and match date on replay list view page

--- hsreplayarchive/web/views.py
+++ hsreplayarchive/web/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from .models import HSReplaySingleGameFileUpload
+from hsreplayparser.parser import HSReplayParser
+from datetime import date
 
 
 def home(request):
@@ -32,10 +34,31 @@ class ReplayUploadView(View):
 
 		data = request.body.decode("utf-8")
 		try:
-			replay = HSReplaySingleGameFileUpload.objects.create(data=request.body)
-			replay.save()
+			parser = HSReplayParser()
+			parser.parse_data(data, is_final=True)
 
-			response['Location'] = '%s://%s/api/v1/replay/%s' % (request.scheme, request.get_host(), replay.id)
+			if len(parser.replay.games) != 1:
+				# Raise an error
+				pass
+
+			game = parser.replay.games[0]
+
+			upload = HSReplaySingleGameFileUpload.objects.create(data=request.body)
+
+			if game.first_player.name:
+				upload.player_1_name = game.first_player.name
+
+			if game.second_player.name:
+				upload.player_2_name = game.second_player.name
+
+			if game.match_date:
+				upload.match_date = game.match_date
+			else:
+				upload.match_date = date.today()
+
+			upload.save()
+
+			response['Location'] = '%s://%s/api/v1/replay/%s' % (request.scheme, request.get_host(), upload.id)
 			response.status_code = 201
 		except Exception as e:
 			response.status_code = 500
