3f5fe1e9295cfc2438404ea34289f048d2545403
Andrew Wilson <andrewmoorewilson@gmail.com>
2016-02-27 12:44:59 +0700

    Add reset password by email system

--- hsreplayarchive/user/urls.py
+++ hsreplayarchive/user/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import AuthenticationForm
 from django.views.generic import RedirectView
 from django.core.urlresolvers import reverse_lazy
-
+from .views import DisableAccount
 
 password_urls = [
 	url(r'^change/$',
@@ -13,6 +13,24 @@ password_urls = [
 		name ='pw_change'),
 	url(r'^change/done/$', auth_views.password_change_done,
 		{'template_name':'user/password_change_done.html'}, name='pw_change_done'),
+	url(r'^reset/$', auth_views.password_reset,
+		{'template_name':'user/password_reset_form.html',
+		 'email_template_name': 'user/password_reset_email.txt',
+		 'subject_template_name': 'user/password_reset_subject.txt',
+		 'post_reset_redirect': reverse_lazy('dj-auth:pw_reset_sent')},
+		name='pw_reset_start'),
+	url(r'^reset/sent/$', auth_views.password_reset_done,
+		{'template_name': 'user/password_reset_sent.html'}, name='pw_reset_sent'),
+	url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+		auth_views.password_reset_confirm,
+		{'template_name': 'user/password_reset_confirm.html',
+		 'post_reset_redirect': reverse_lazy('dj-auth:pw_reset_complete')},
+		name='pw_reset_confirm'),
+	url(r'^reset/done/$', auth_views.password_reset_complete,
+		{'template_name':'user/password_reset_complete.html',
+		 'extra_context': {'form': AuthenticationForm}},
+		name='pw_reset_complete'),
+	url(r'^disable/$', DisableAccount.as_view(), name='disable'),
 ]
 
 
--- hsreplayarchive/user/views.py
+++ hsreplayarchive/user/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.views.generic import View
+from django.contrib.auth import get_user, logout
+from django.contrib.auth.decorators import login_required
+from django.template.response import TemplateResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_protect
 
-# Create your views here.
+
+class DisableAccount(View):
+	success_url = settings.LOGIN_REDIRECT_URL
+	template_name = 'user/user_confirm_delete.html'
+
+	@method_decorator(csrf_protect)
+	@method_decorator(login_required)
+	def get(self, request):
+		return TemplateResponse(request, self.template_name)
+
+	@method_decorator(csrf_protect)
+	@method_decorator(login_required)
+	def post(self, request):
+		user = get_user(request)
+		user.set_unusable_password()
+		user.is_active = False
+		user.save()
+		logout(request)
+		return redirect(self.success_url)
