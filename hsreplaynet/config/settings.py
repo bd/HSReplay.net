832433d6e490b162725466e954428c4843cdfb9b
Andrew Wilson <andrewmoorewilson@gmail.com>
2016-04-16 23:09:06 +0700

    Cleanup old django_storages

--- hsreplaynet/config/settings.py
+++ hsreplaynet/config/settings.py
@@ -31,7 +31,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'storages',
     'web',
     'joust',
     'allauth',
@@ -89,26 +88,7 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-# The following section based on: https://www.caktusgroup.com/blog/2014/11/10/Using-Amazon-S3-to-store-your-Django-sites-static-and-media-files/
 
-# Uncomment this block to instruct S3 that when it serves objects it can instruct browsers to cache them for a LONG time.
-# AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
-#     'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-#     'Cache-Control': 'max-age=94608000',
-# }
-
-
-# AWS_IAM_USER = 'arn:aws:iam::272503103573:user/hsreplayarchiveuser'
-AWS_STORAGE_BUCKET_NAME = 'replays.hsreplayarchive.org'
-AWS_ACCESS_KEY_ID = 'AKIAINP23BGAX74MXI5A'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_S3_CALLING_FORMAT = 'boto.s3.connection.OrdinaryCallingFormat'
-AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME
-AWS_S3_SECURE_URLS = False
-
-#STATICFILES_LOCATION = 'static'
-#STATICFILES_STORAGE = 'config.storage.StaticStorage'
-#STATIC_URL = "http://%s/%s/" % (AWS_STORAGE_BUCKET_NAME, STATICFILES_LOCATION)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_HOST = 'http://static.hsreplay.net' if not DEBUG else ''
@@ -116,10 +96,6 @@ STATIC_URL = STATIC_HOST + '/static/'
 
 if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-MEDIAFILES_LOCATION = 'media'
-MEDIA_URL = "http://%s/%s/" % (AWS_STORAGE_BUCKET_NAME, MEDIAFILES_LOCATION)
-DEFAULT_FILE_STORAGE = 'config.storage.MediaStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
@@ -133,7 +109,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'hsreplaynet',
-        'USER': os.environ.get('HSREPLAYNET_DB_USER', 'root'),
+        'USER': os.environ.get('HSREPLAYNET_DB_USER', 'django'),
         'PASSWORD': os.environ.get('HSREPLAYNET_DB_PASSWORD', ''),
         'HOST': os.environ.get('HSREPLAYNET_DB_HOST', 'localhost'),
         'PORT': os.environ.get('HSREPLAYNET_DB_PORT', ''),
