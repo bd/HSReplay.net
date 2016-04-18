d6f0cf3d6127c87cda4429b44664b60dbbe3930d
Andrew Wilson <andrewmoorewilson@gmail.com>
2016-04-18 14:59:52 +0700

    Updates to deployment script for running as www-data.

--- deploy/README.md
+++
@@ -1,3 +0,0 @@
-The command to deploy is:
-
-# fab deploy:host=ubuntu@metacurve.net -i ~/.ssh/tdd.pem
--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -1,14 +1,16 @@
 from fabric.contrib.files import append, exists, sed
 from fabric.api import env, local, run, sudo
 import random, time
+from fabric.context_managers import shell_env
+import os
 
 REPO_URL = 'https://github.com/amw2104/hsreplaynet'
 
 def deploy():
-    site_folder = '/home/metacurve/sites/hsreplay.net'
+    site_folder = '/srv/http/hsreplay.net'
     source_folder = site_folder + '/source'
-    _create_directory_structure_if_necessary(site_folder)
     _get_latest_source(source_folder)
+    _create_directory_structure_if_necessary(site_folder)
     _update_settings(source_folder, 'hsreplay.net')
     _update_virtualenv(source_folder)
     _update_static_files(source_folder)
@@ -17,17 +19,17 @@ def deploy():
 
 
 def _create_directory_structure_if_necessary(site_folder):
-    sudo('mkdir -p %s/source' % (site_folder,), user='metacurve')
-    sudo('mkdir -p %s/source/virtualenv' % (site_folder,), user='metacurve')
+    sudo('mkdir -p %s/source' % (site_folder,), user='www-data')
+    sudo('mkdir -p %s/source/virtualenv' % (site_folder,), user='www-data')
 
 
 def _get_latest_source(source_folder):
     if exists(source_folder + '/.git'):
-        sudo('cd %s && git fetch' % (source_folder,), user='metacurve')
+        sudo('cd %s && git fetch' % (source_folder,), user='www-data')
     else:
-        sudo('git clone %s %s' % (REPO_URL, source_folder), user='metacurve')
+        sudo('git clone %s %s' % (REPO_URL, source_folder), user='www-data')
     current_commit = local("git log -n 1 --format=%H", capture=True)
-    sudo('cd %s && git reset --hard %s' % (source_folder, current_commit), user='metacurve')
+    sudo('cd %s && git reset --hard %s' % (source_folder, current_commit), user='www-data')
 
 
 def _update_settings(source_folder, site_name):
@@ -45,17 +47,22 @@ def _update_settings(source_folder, site_name):
 def _update_virtualenv(source_folder):
     virtualenv_folder = source_folder + '/virtualenv'
     if not exists(virtualenv_folder + '/bin/pip'):
-        sudo('virtualenv --python=python3 %s' % (virtualenv_folder,), user='metacurve')
-    sudo('%s/bin/pip install -r %s/requirements.txt --allow-external mysql-connector-python' % (virtualenv_folder, source_folder), user='metacurve')
+        sudo('virtualenv --python=python3 %s' % (virtualenv_folder,), user='www-data')
+    sudo('%s/bin/pip install -r %s/requirements.txt' % (virtualenv_folder, source_folder), user='www-data')
 
 
 def _update_static_files(source_folder):
-    sudo('cd %s/hsreplaynet && ../virtualenv/bin/python manage.py collectstatic --noinput' % (source_folder,), user='metacurve')
+    sudo('cd %s/hsreplaynet && ../virtualenv/bin/python manage.py collectstatic --noinput' % (source_folder,), user='www-data')
 
 
 def _update_database(source_folder):
-    sudo('cd %s/hsreplaynet && ../virtualenv/bin/python manage.py migrate --noinput' % (source_folder,), user='metacurve')
+    with shell_env(HSREPLAYNET_DB_USER=os.environ.get('HSREPLAYNET_DB_USER'),
+                   HSREPLAYNET_DB_PASSWORD=os.environ.get('HSREPLAYNET_DB_PASSWORD'),
+                   HSREPLAYNET_DB_HOST=os.environ.get('HSREPLAYNET_DB_HOST'),
+                   HSREPLAYNET_DB_PORT=os.environ.get('HSREPLAYNET_DB_PORT')):
+
+        sudo('cd %s/hsreplaynet && ../virtualenv/bin/python manage.py migrate --noinput' % (source_folder,), user='www-data')
 
 
 def _restart_web_server():
-    sudo('restart gunicorn-hsreplay.net', user='metacurve')
+    sudo('restart gunicorn-hsreplay.net', user='www-data')
