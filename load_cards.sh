e6f9ea7961080a2aa83d2cbb7a29c10fb6d64a73
Andrew Wilson <andrewmoorewilson@gmail.com>
2016-05-01 18:14:52 -0400

    Give nightly card loading script access to production environment variables.

--- load_cards.sh
+++ load_cards.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source /srv/http/hsreplay.net/source/virtualenv/bin/activate
+source /srv/http/hsreplay.net/source/bin/set_production_environment_vars.sh
 python /srv/http/hsreplay.net/source/hsreplaynet/manage.py load_cards
