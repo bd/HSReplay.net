70cec6003a9dd3e6f496337f6ddc157502ee4f3c
Andrew Wilson <andrewmoorewilson@gmail.com>
2016-05-10 15:40:25 +0200

    Update Lambda deployment script to include hsreplay dependency.

--- deploy/build_lambdas.sh
+++ deploy/build_lambdas.sh
@@ -5,4 +5,6 @@ cd ../hsreplaynet
 zip -r $cwd/hsreplay.zip ./*
 cd $VIRTUAL_ENV/lib/python2.7/site-packages
 zip -r $cwd/hsreplay.zip ./*
-
+hsreplay_lib_path=$(cat hsreplay.egg-link | head -n 1)
+cd $hsreplay_lib_path
+zip -r $cwd/hsreplay.zip ./hsreplay/*
