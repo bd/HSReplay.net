aef4d04d784cafbc5bcd9abe8ca1154f8286babb
Andrew Wilson <andrewmoorewilson@gmail.com>
2016-05-11 18:36:23 +0200

    Make load_cards management command use timezone aware datetimes.

--- hsreplaynet/cards/management/commands/load_cards.py
+++ hsreplaynet/cards/management/commands/load_cards.py
@@ -1,14 +1,14 @@
 from django.core.management.base import BaseCommand
 from cards.loader import HearthstoneApiLoader
 from cards.models import CardCollectionAuditLog
-from datetime import date, datetime
-
+from datetime import date
+from django.utils.timezone import now
 
 class Command(BaseCommand):
 	def handle(self, *args, **options):
 		audit_log = CardCollectionAuditLog()
 		audit_log.job_date = date.today()
-		audit_log.card_collection_start = datetime.now()
+		audit_log.card_collection_start = now()
 		audit_log.save()
 
 		try:
@@ -16,12 +16,12 @@ class Command(BaseCommand):
 			result = loader.load()
 			audit_log.num_new_cards_loaded = result.num_new_cards_created
 			audit_log.card_collection_succeeded = True
-			audit_log.card_collection_end = datetime.now()
+			audit_log.card_collection_end = now()
 			audit_log.save()
 
 		except Exception as e:
 
 			audit_log.card_collection_succeeded = False
 			audit_log.exception_text = str(e)
-			audit_log.card_collection_end = datetime.now()
+			audit_log.card_collection_end = now()
 			audit_log.save()
