a932ea5e35b96982fc4eb4fb8fdf176530cb7506
Jerome Leclanche <jerome@leclan.ch>
2016-05-12 17:05:46 +0300

    Upgrade python-hearthstone from load_cards.sh

--- load_cards.sh
+++ load_cards.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source /srv/http/hsreplay.net/source/virtualenv/bin/activate
 source /srv/http/hsreplay.net/source/bin/set_production_environment_vars.sh
+pip install --upgrade hearthstone
 python /srv/http/hsreplay.net/source/hsreplaynet/manage.py load_cards
