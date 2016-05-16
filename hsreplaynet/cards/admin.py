b65ba7993a0c1b394ce74a60f4710042d6dcbaa1
Jerome Leclanche <jerome@leclan.ch>
2016-05-16 19:12:55 +0300

    Add administration module for cards.*

--- hsreplaynet/cards/admin.py
+++ hsreplaynet/cards/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+	list_display = (
+		"__str__", "id", "description", "card_set", "rarity", "type",
+		"card_class", "artist"
+	)
+	list_filter = (
+		"collectible", "card_set",
+		"card_class", "type", "rarity", "cost",
+		"battlecry", "deathrattle", "inspire",
+		"divine_shield", "taunt", "windfury",
+		"overload", "spell_damage"
+	)
+	search_fields = (
+		"name", "description", "id",
+	)
+
+
+class IncludeInline(admin.TabularInline):
+	model = Include
+	raw_id_fields = ("card", )
+	extra = 15
+
+
+@admin.register(Deck)
+class DeckAdmin(admin.ModelAdmin):
+	date_hierarchy = "created"
+	inlines = (IncludeInline, )
