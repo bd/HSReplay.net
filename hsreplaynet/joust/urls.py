ad106b746064940ca9cd2b56e78e1be879d668a3
Jerome Leclanche <jerome@leclan.ch>
2016-05-16 09:15:36 +0300

    Make the My Replays redirect nonpermanent

--- hsreplaynet/joust/urls.py
+++ hsreplaynet/joust/urls.py
@@ -4,7 +4,7 @@ from .views import *
 
 
 urlpatterns = [
-	url(r"^$", RedirectView.as_view(pattern_name="my_replays")),
+	url(r"^$", RedirectView.as_view(pattern_name="my_replays", permanent=False)),
 	url(r"^mine/$", JoustPrivateCollectionView.as_view(), name="my_replays"),
 	url(r"^replay/(?P<id>[\w-]+)$", ReplayDetailView.as_view(), name="joust_replay_view"),
 ]
