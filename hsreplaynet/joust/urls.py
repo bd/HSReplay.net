c43fdc4529810f49c521c008dac764bef4c2a961
Jerome Leclanche <jerome@leclan.ch>
2016-05-16 08:28:13 +0300

    Drop public replay view and redirect to My Replays

--- hsreplaynet/joust/urls.py
+++ hsreplaynet/joust/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.views.generic import RedirectView
 from .views import *
 
+
 urlpatterns = [
-    url(r'^$', JoustStartupView.as_view(), name='joust_replay_list'),
-    url(r'^collection/$', JoustPrivateCollectionView.as_view(), name='private_replay_collection'),
-    url(r'^replay/(?P<id>[\w-]+)$', ReplayDetailView.as_view(), name='joust_replay_view'),
+	url(r"^$", RedirectView.as_view(pattern_name="private_replay_collection")),
+	url(r"^collection/$", JoustPrivateCollectionView.as_view(), name="private_replay_collection"),
+	url(r"^replay/(?P<id>[\w-]+)$", ReplayDetailView.as_view(), name="joust_replay_view"),
 ]
--- hsreplaynet/joust/views.py
+++ hsreplaynet/joust/views.py
@@ -1,38 +1,23 @@
 from django.shortcuts import render
 from django.views.generic import View
-
 from web.models import *
 
 
-class JoustStartupView(View):
-
-	def get(self, request):
-
-		if request.user.is_authenticated() and request.user.is_staff:
-			replays = GameReplayUpload.objects.order_by('-global_game__match_start_timestamp').all()
-		else:
-			replays = GameReplayUpload.objects.order_by('-global_game__match_start_timestamp').filter(is_public=True).all()
-
-		context = {'replays': replays }
-		return render(request, 'joust/public_replay_list.html', context)
-
-
 class JoustPrivateCollectionView(View):
-
 	def get(self, request):
 		replays = []
 
 		for token in request.user.tokens.all():
 			replays.extend(list(GameReplayUpload.objects.filter(upload_token=token).all()))
 
-		sorted_replays = sorted(replays, key=lambda r: r.global_game.match_start_timestamp )
+		sorted_replays = sorted(replays, key=lambda r: r.global_game.match_start_timestamp)
 
-		context = {'replays': sorted_replays, 'count': len(sorted_replays) }
-		return render(request, 'joust/private_replay_collection.html', context)
+		context = {"replays": sorted_replays, "count": len(sorted_replays)}
+		return render(request, "joust/private_replay_collection.html", context)
 
 
 class ReplayDetailView(View):
-
 	def get(self, request, id):
-
-		return render(request, 'joust/replay_detail.html', {'replay_data_url': '/api/v1/replay/%s' % id})
+		return render(request, "joust/replay_detail.html", {
+			"replay_data_url": "/api/v1/replay/%s" % (id),
+		})
