8ee802909b480dabc3bf48f4bc9280cac7ad6f41
Jerome Leclanche <jerome@leclan.ch>
2016-05-16 08:48:47 +0300

    Mark the My Replays view as login_required

--- hsreplaynet/joust/views.py
+++ hsreplaynet/joust/views.py
@@ -1,9 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views.generic import View
 from web.models import *
 
 
 class JoustPrivateCollectionView(View):
+	@method_decorator(login_required)
+	def dispatch(self, *args, **kwargs):
+		return super().dispatch(*args, **kwargs)
+
 	def get(self, request):
 		replays = []
 
