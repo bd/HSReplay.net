915ed5565cb409a90cc5ec9d7b7dfd634d057097
Jerome Leclanche <jerome@leclan.ch>
2016-05-17 15:32:05 +0300

    Add a base "edit account" page

--- hsreplaynet/config/urls.py
+++ hsreplaynet/config/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 from web.urls import api, docs
 
 
@@ -9,6 +10,10 @@ urlpatterns = [
 	url(r"^api/", include(api)),
 	url(r"^docs/", include(docs)),
 	url(r"^games/", include("joust.urls")),
+	url(r"^account/$",
+		TemplateView.as_view(template_name="account/edit.html"),
+		name="account_edit"
+	),
 	url(r"^account/", include("allauth_battlenet.urls")),
 	url(r"^account/", include("allauth.urls")),
 	url(r"^", include("django.contrib.flatpages.urls")),
