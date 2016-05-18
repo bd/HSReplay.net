ac37a06e1fd4324b75dbd89a78cedf6d3c3b0fca
Benedict Etzel <developer@beheh.de>
2016-05-18 12:01:01 +0200

    Replay collection: Make timestamp natural time

--- hsreplaynet/web/templatetags/web_extras.py
+++ hsreplaynet/web/templatetags/web_extras.py
@@ -1,5 +1,6 @@
 from django import template
-from humanize import naturaldelta
+from humanize import naturaldelta, naturaltime
+from datetime import datetime
 
 
 register = template.Library()
@@ -8,3 +9,7 @@ register = template.Library()
 @register.filter
 def human_duration(value):
 	return naturaldelta(value)
+
+@register.filter
+def human_time(value):
+	return naturaltime(datetime.now(value.tzinfo) - value)
