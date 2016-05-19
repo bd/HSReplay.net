580056e48069dd9f7abe049fb91606bbdeb61e34
Jerome Leclanche <jerome@leclan.ch>
2016-05-19 05:55:09 +0300

    Add Privacy Policy and Terms of Service as builtin URLs

--- hsreplaynet/config/urls.py
+++ hsreplaynet/config/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.flatpages.views import flatpage
 from django.views.generic import TemplateView
 from web.urls import api, docs
 from web.views import home
@@ -11,6 +12,8 @@ urlpatterns = [
 	url(r"^api/", include(api)),
 	url(r"^docs/", include(docs)),
 	url(r"^games/", include("joust.urls")),
+	url(r"^about/privacy/$", flatpage, {"url": "/about/privacy/"}, name="privacy_policy"),
+	url(r"^about/tos/$", flatpage, {"url": "/about/tos/"}, name="terms_of_service"),
 	url(r"^account/$",
 		TemplateView.as_view(template_name="account/edit.html"),
 		name="account_edit"
