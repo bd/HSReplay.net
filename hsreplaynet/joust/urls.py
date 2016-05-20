9ce1aa8031a34a7295d2d8c4955b467b9065f0df
Jerome Leclanche <jerome@leclan.ch>
2016-05-20 19:56:37 +0300

    Delete unnecessary files from hsreplaynet.joust

--- hsreplaynet/joust/urls.py
+++ hsreplaynet/joust/urls.py
@@ -5,6 +5,6 @@ from .views import *
 
 urlpatterns = [
 	url(r"^$", RedirectView.as_view(pattern_name="my_replays", permanent=False)),
-	url(r"^mine/$", JoustPrivateCollectionView.as_view(), name="my_replays"),
+	url(r"^mine/$", MyReplaysView.as_view(), name="my_replays"),
 	url(r"^replay/(?P<id>[\w-]+)$", ReplayDetailView.as_view(), name="joust_replay_view"),
 ]
--- hsreplaynet/joust/views.py
+++ hsreplaynet/joust/views.py
@@ -2,10 +2,10 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.generic import View
-from web.models import *
+from web.models import GameReplayUpload
 
 
-class JoustPrivateCollectionView(View):
+class MyReplaysView(View):
 	@method_decorator(login_required)
 	def dispatch(self, *args, **kwargs):
 		return super().dispatch(*args, **kwargs)
