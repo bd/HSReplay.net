bdcef08bb819782a9a85ba834dc4b8d1de431a1e
Jerome Leclanche <jerome@leclan.ch>
2016-05-22 09:38:08 +0300

    Strictify permissions on auth tokens / api keys

--- hsreplaynet/api/views.py
+++ hsreplaynet/api/views.py
@@ -1,15 +1,20 @@
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin
 from rest_framework.permissions import AllowAny
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import GenericViewSet
 from .models import AuthToken, UploadAgentAPIKey
 from .serializers import AuthTokenSerializer, UploadAgentSerializer
 
 
-class AuthTokenViewSet(ModelViewSet):
+class WriteOnlyOnceViewSet(CreateModelMixin, RetrieveModelMixin, GenericViewSet):
+	pass
+
+
+class AuthTokenViewSet(WriteOnlyOnceViewSet):
 	permission_classes = (AllowAny, )
 	queryset = AuthToken.objects.all()
 	serializer_class = AuthTokenSerializer
 
 
-class UploadAgentViewSet(ModelViewSet):
+class UploadAgentViewSet(WriteOnlyOnceViewSet):
 	queryset = UploadAgentAPIKey.objects.all()
 	serializer_class = UploadAgentSerializer
