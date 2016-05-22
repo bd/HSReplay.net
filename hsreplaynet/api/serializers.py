ad2fcbdc4391f3e030784d015fc8772e9776b499
Jerome Leclanche <jerome@leclan.ch>
2016-05-22 08:37:41 +0300

    Only require an API Key when creating an Auth Token

--- hsreplaynet/api/serializers.py
+++ hsreplaynet/api/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework.exceptions import ValidationError
 from rest_framework import serializers
 from .models import AuthToken, UploadAgentAPIKey
 
@@ -11,10 +12,20 @@ class UserSerializer(serializers.Serializer):
 class AuthTokenSerializer(serializers.HyperlinkedModelSerializer):
 	key = serializers.CharField(read_only=True)
 	user = UserSerializer(read_only=True)
+	api_key = serializers.CharField(write_only=True)
 
 	class Meta:
 		model = AuthToken
-		fields = ("key", "user")
+		fields = ("key", "user", "api_key")
+
+	def create(self, data):
+		try:
+			api_key = UploadAgentAPIKey.objects.get(api_key=data.pop("api_key"))
+		except (UploadAgentAPIKey.DoesNotExist, ValueError) as e:
+			raise ValidationError(str(e))
+		ret = super(AuthTokenSerializer, self).create(data)
+		api_key.tokens.add(ret)
+		return ret
 
 
 class UploadAgentSerializer(serializers.HyperlinkedModelSerializer):
--- hsreplaynet/api/views.py
+++ hsreplaynet/api/views.py
@@ -1,9 +1,11 @@
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 from .models import AuthToken, UploadAgentAPIKey
 from .serializers import AuthTokenSerializer, UploadAgentSerializer
 
 
 class AuthTokenViewSet(ModelViewSet):
+	permission_classes = (AllowAny, )
 	queryset = AuthToken.objects.all()
 	serializer_class = AuthTokenSerializer
 
