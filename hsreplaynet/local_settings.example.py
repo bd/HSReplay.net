8116745489f5c2c33a80e062b13f10d81269975e
Jerome Leclanche <jerome@leclan.ch>
2016-05-24 10:27:30 +0300

    Add more useful settings to example local_settings.py

--- hsreplaynet/local_settings.example.py
+++ hsreplaynet/local_settings.example.py
@@ -1,4 +1,5 @@
 DEBUG = True
+ALLOWED_HOSTS = ["locahost:8000"]
 
 ACCOUNT_DEFAULT_HTTP_PROTOCOL = "http"
 
@@ -7,3 +8,8 @@ STATIC_URL = "/static/"
 JOUST_STATIC_URL = "//static.hsreplay.net/static/joust/"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+INTERNAL_IPS = (
+	"0.0.0.0",
+	"127.0.0.1",
+)
