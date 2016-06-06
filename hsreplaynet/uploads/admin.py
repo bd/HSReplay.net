78ed67210797cd5c0f3e401474b2976fc89899bf
Jerome Leclanche <jerome@leclan.ch>
2016-06-06 21:47:04 +0300

    Add an admin action to reprocess upload events

--- hsreplaynet/uploads/admin.py
+++ hsreplaynet/uploads/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from hsreplaynet.utils.admin import admin_urlify as urlify
 from .models import UploadEvent
+from .processing import queue_upload_event_for_processing
+
+
+def queue_for_reprocessing(admin, request, queryset):
+	for obj in queryset:
+		queue_upload_event_for_processing(str(obj.id))
+queue_for_reprocessing.short_description = "Queue for reprocessing"
 
 
 @admin.register(UploadEvent)
 class UploadEventAdmin(admin.ModelAdmin):
+	actions = (queue_for_reprocessing, )
 	date_hierarchy = "created"
 	list_display = (
 		"__str__", "status", "tainted", "type", urlify("token"),
