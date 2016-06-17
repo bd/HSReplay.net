6edc1394d845b4493a7c79dc919dcbf29a1f6dfb
Jerome Leclanche <jerome@leclan.ch>
2016-06-17 09:31:56 +0300

    Add a management command to ingest a Power.log file
    Closes #4

---
+++ hsreplaynet/games/management/commands/ingest_log.py
@@ -0,0 +1,34 @@
+import json
+from django.core.files import File
+from django.core.management.base import BaseCommand
+from django.utils.timezone import now
+from hsreplaynet.uploads.models import UploadEvent, UploadEventType
+
+
+class Command(BaseCommand):
+	def add_arguments(self, parser):
+		parser.add_argument("file", nargs="+")
+
+	def handle(self, *args, **options):
+		for file in options["file"]:
+			print("Uploading %r" % (file))
+			metadata = {
+				"hearthstone_build": -1,
+				"match_start_timestamp": now().isoformat()
+			}
+
+			event = UploadEvent(
+				type=UploadEventType.POWER_LOG,
+				upload_ip="127.0.0.1",
+				metadata=json.dumps(metadata),
+			)
+
+			event.file = file
+			event.save()
+
+			with open(file, "r") as f:
+				event.file = File(f)
+				event.save()
+
+			event.process()
+			self.stdout.write("%r: %s" % (event, event.get_absolute_url()))
