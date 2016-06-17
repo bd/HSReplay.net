18d6cdc7afe6cc597c21421a47bbef6395c7b023
Jerome Leclanche <jerome@leclan.ch>
2016-06-17 12:31:22 +0300

    Use a UUIDField for AccountClaim model

--- hsreplaynet/accounts/models.py
+++ hsreplaynet/accounts/models.py
@@ -1,17 +1,17 @@
+import uuid
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from hsreplaynet.utils import generate_key
 
 
 class AccountClaim(models.Model):
-	id = models.CharField(max_length=40, primary_key=True)
+	id = models.UUIDField(primary_key=True)
 	token = models.OneToOneField("api.AuthToken")
 	created = models.DateTimeField("Created", auto_now_add=True)
 
 	def save(self, *args, **kwargs):
 		if not self.id:
-			self.id = generate_key()
+			self.id = uuid.uuid4()
 		return super().save(*args, **kwargs)
 
 	def __str__(self):
--- hsreplaynet/accounts/urls.py
+++ hsreplaynet/accounts/urls.py
@@ -5,6 +5,6 @@ from .views import ClaimAccountView
 
 urlpatterns = [
 	url(r"^$", TemplateView.as_view(template_name="account/edit.html"), name="account_edit"),
-	url(r"^claim/(?P<id>[\w]+)/$", ClaimAccountView.as_view(), name="account_claim"),
+	url(r"^claim/(?P<id>[\w-]+)/$", ClaimAccountView.as_view(), name="account_claim"),
 	url(r"^delete/$", TemplateView.as_view(template_name="account/delete.html"), name="account_delete"),
 ]
