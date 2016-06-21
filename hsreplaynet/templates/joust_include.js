0e0b94d52f4e624b04ce9cb9367a52a1d14b905c
Jerome Leclanche <jerome@leclan.ch>
2016-06-21 11:46:21 +0300

    Use model-based loading for the featured replay

---
+++ hsreplaynet/templates/joust_include.js
@@ -0,0 +1,62 @@
+{% load static from staticfiles %}
+{% load web_extras %}
+
+var joust_required = [
+	"{% joust_static 'joust.css' %}",
+	"https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react.min.js",
+	"https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react-dom.min.js",
+	"{% joust_static 'joust.js' %}",
+	"{% static 'web/joust-extra.js' %}"
+];
+
+$(document).ready(function() {
+	$('#joust-lightbox').click(function(e) {
+		e.preventDefault();
+		$('#joust-lightbox').hide();
+	}).children().click(function(e) {
+	  return false;
+	});
+	var joust_started = false;
+	var joust_check = function() {
+		if(joust_required.length) {
+			var file = joust_required.shift();
+			var tag = file.match(/\.css$/) ? 'link' : 'script';
+			var element = document.createElement(tag);
+			element.onload = function() {
+				joust_check();
+			};
+			if(tag == 'link') {
+				element.href = file;
+				element.rel = 'stylesheet';
+			}
+			else {
+				element.src = file;
+			}
+			document.getElementsByTagName('head')[0].appendChild(element);
+		}
+		else if(!joust_started) {
+			joust_started = true;
+
+			var shim = document.createElement('style');
+			shim.innerText = "{% filter escapejs %}{% include 'games/svg-paths-shim.css' with svg='/static/web/svg-paths.svg' %}{% endfilter %}";
+			document.getElementsByTagName('head')[0].appendChild(shim);
+
+			JoustExtra.setup({
+				hearthstonejson: "{% setting 'HEARTHSTONEJSON_URL' %}"
+			});
+			Joust.viewer("joust-promo-container")
+				.metadata(JoustExtra.metadata)
+				.assets("{% joust_static 'assets/' %}")
+				.cardArt("{% joust_static 'card-art/' %}")
+				.width("100%")
+				.height("100%")
+				.fromUrl("{{ featured_game.replay_xml.url }}");
+		}
+	};
+	$('#feat-joust-screenshot').click(function(e) {
+		e.preventDefault();
+		$('#joust-lightbox').fadeIn();
+		joust_check();
+	});
+});
+
--- hsreplaynet/utils/templatetags/web_extras.py
+++ hsreplaynet/utils/templatetags/web_extras.py
@@ -2,6 +2,7 @@ from django import template
 from django.conf import settings
 from humanize import naturaldelta, naturaltime
 from datetime import datetime
+from hsreplaynet.games.models import GameReplay
 
 
 register = template.Library()
@@ -20,6 +21,19 @@ def human_time(value):
 @register.simple_tag
 def joust_static(path):
 	return settings.JOUST_STATIC_URL + path
+
+
+@register.simple_tag
+def get_featured_game():
+	id = getattr(settings, "FEATURED_GAME_ID", None)
+	if not id:
+		return
+
+	try:
+		replay = GameReplay.objects.get(shortid=id)
+	except GameReplay.DoesNotExist:
+		replay = None
+	return replay
 
 
 @register.simple_tag(takes_context=True)
