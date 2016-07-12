3a8770be362b5da39f309a13a2d03af2f07e364d
Jerome Leclanche <jerome@leclan.ch>
2016-07-12 20:04:04 +0300

    Improve Scenario administration

--- hsreplaynet/scenarios/admin.py
+++ hsreplaynet/scenarios/admin.py
@@ -2,11 +2,30 @@ from django.contrib import admin
 from .models import Adventure, Wing, Scenario
 
 
+class ScenarioInline(admin.TabularInline):
+	model = Scenario
+	fields = ("name", "note_desc", "adventure", "wing", "mode")
+	readonly_fields = fields
+	extra = 0
+	show_change_link = True
+
+
+class WingInline(admin.TabularInline):
+	model = Wing
+	fields = (
+		"name", "note_desc", "ownership_prereq_wing", "coming_soon_label"
+	)
+	readonly_fields = fields
+	extra = 0
+	show_change_link = True
+
+
 @admin.register(Adventure)
 class AdventureAdmin(admin.ModelAdmin):
 	list_display = ("__str__", "note_desc")
 	list_filter = ("leaving_soon", )
 	search_fields = ("name", "note_desc")
+	inlines = (WingInline, ScenarioInline, )
 
 
 @admin.register(Wing)
@@ -17,6 +36,7 @@ class WingAdmin(admin.ModelAdmin):
 	)
 	list_filter = ("adventure", )
 	search_fields = ("name", "note_desc")
+	inlines = (ScenarioInline, )
 
 
 @admin.register(Scenario)
@@ -25,5 +45,7 @@ class ScenarioAdmin(admin.ModelAdmin):
 		"__str__", "note_desc", "is_tutorial", "is_expert", "is_coop",
 		"players", "adventure", "wing", "mode", "opponent_name",
 	)
-	list_filter = ("players", "is_tutorial", "is_expert", "is_coop", "mode")
+	list_filter = (
+		"adventure", "players", "is_tutorial", "is_expert", "is_coop", "mode"
+	)
 	search_fields = ("name", "note_desc", "description", "opponent_name")
